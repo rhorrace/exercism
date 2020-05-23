9b0e6cfb893701fad244294429a6576854c92e81
rhorrace <rhorrace@pdx.edu>
2020-05-22 22:09:24 -0500

    python scrabble score

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,12 @@
 def abbreviate(words):
-    pass
+    acronym = ""
+    letters = []
+    for letter in words.lower():
+        if letter.isalnum() or letter == "'":
+            letters += letter
+        else:
+            letters += " "
+    words = "".join(letters).split()
+    for word in words:
+        acronym += word[0]
+    return acronym.upper()
