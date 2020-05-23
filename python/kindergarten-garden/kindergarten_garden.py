92e73ea32cf1eae7e6096d9b5b1cd87e41bbf112
rhorrace <rhorrace@pdx.edu>
2020-05-23 18:42:42 -0500

    python Kindergarten Garden

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,21 @@
 class Garden:
-    def __init__(self, diagram, students):
-        pass
+    __default_students = ["Alice", "Bob", "Charlie",
+                          "David", "Eve", "Fred",
+                          "Ginny", "Harriet", "Ileana",
+                          "Joseph", "Kincaid", "Larry"]
+    __plant_dict = {'R': "Radishes", 'C': "Clover",
+                    'G': "Grass", 'V': "Violets"}
+
+    def __init__(self, diagram, students=__default_students):
+        plants_1, plants_2 = diagram.split()
+        self.__student_plants = {}
+        students = sorted(students)
+        for i, student in enumerate(students):
+            try:
+                self.__student_plants[student] = [plants_1[2 * i], plants_1[2 * i + 1],
+                                                  plants_2[2 * i], plants_2[2 * i + 1]]
+            except IndexError:
+                break
+
+    def plants(self, name):
+        return list(map(lambda s: self.__plant_dict[s], self.__student_plants[name]))
