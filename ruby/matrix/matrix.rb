6dece1923f1effcfea96adc1feed874f9bb888ef
rhorrace <rhorrace@pdx.edu>
2020-05-25 13:44:39 -0500

    ruby matrix

---
+++ ruby/matrix/matrix.rb
@@ -0,0 +1,23 @@
+=begin
+Write your code for the 'Matrix' exercise in this file. Make the tests in
+`matrix_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/matrix` directory.
+=end
+
+class Matrix
+
+    attr_reader :rows
+
+    def initialize(input)
+        @rows = input.split("\n").map do |row|
+            row.split(' ').map(&:to_i)
+        end
+    end
+
+    def columns
+        @rows.transpose
+    end
+end
+
