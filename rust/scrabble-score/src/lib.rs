061fbcbd3f067e9c7a786d65cf28cf77a567b1b2
rhorrace <rhorrace@pdx.edu>
2020-06-04 21:55:58 -0500

    rust scrabble score

---
+++ rust/scrabble-score/src/lib.rs
@@ -0,0 +1,20 @@
+/// Compute the Scrabble score for a word.
+pub fn score(word: &str) -> u64 {
+    word.to_lowercase()
+        .chars()
+        .map(|c| calculate(c))
+        .sum::<u64>()
+}
+
+fn calculate(c: char) -> u64 {
+    match c {
+        'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' => 1,
+        'd' | 'g' => 2,
+        'b' | 'c' | 'm' | 'p' => 3,
+        'f' | 'h' | 'v' | 'w' | 'y' => 4,
+        'k' => 5,
+        'j' | 'x' => 8,
+        'q' | 'z' => 10,
+        _ => 0,
+    }
+}
