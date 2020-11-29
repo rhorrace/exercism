a674b58de09eced49cf6fd4bcdcdb9b0730654a3
rhorrace <rhorrace@pdx.edu>
2020-11-29 10:44:05 -0700

    ruby resistor color

---
+++ ruby/resistor-color/resistor_color.rb
@@ -0,0 +1,16 @@
+=begin
+Write your code for the 'Resistor Color' exercise in this file. Make the tests in
+`resistor_color_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/resistor-color` directory.
+=end
+
+module ResistorColor
+
+  def self.color_code(color)
+
+  end
+
+  COLORS
+end
